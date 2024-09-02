b675b9406cdae6687b88539686d1fdaed297917f
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2024-09-02 10:18:17 +0530

    url update

--- src/components/admin/Battles.js
+++ src/components/admin/Battles.js
@@ -456,7 +456,7 @@ const BattlesComponent = ({ initialTab = 0 }) => {
   };
 
   const setSocketIo = () => {
-    const socketIo = io("http://93.127.198.250:3002");
+    const socketIo = io("https://socket.aoneludo.com");
     setSocket(socketIo);
     if (socketIo.connected) {
       onConnect();
--- src/pages/battles.js
+++ src/pages/battles.js
@@ -59,7 +59,7 @@ const CreateBattle = () => {
   const [socket, setSocket] = useState(null);
 
   const setSocketIo = () => {
-    const socketIo = io("http://93.127.198.250:3002");
+    const socketIo = io("https://socket.aoneludo.com");
     setSocket(socketIo);
     if (socketIo.connected) {
       onConnect();
