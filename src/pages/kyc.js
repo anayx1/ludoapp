b50ef2a8532c5e97146bd392efedbd1d3a04d92f
anayx1 <anaytiwari@jklu.edu.in>
2024-09-14 17:20:05 +0530

    kyc update

--- src/pages/kyc.js
+++ src/pages/kyc.js
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import axios from "axios";
 import dynamic from "next/dynamic";
+import Router from "next/router";
 
 // Dynamically import the Loader component, disabling SSR
 const Loader = dynamic(() => import("@/components/Loader"), {
@@ -24,6 +25,7 @@ const KycForm = () => {
     back_side: null,
   });
   const [userId, setUserId] = useState(null);
+  const [userKycStatus, setUserKycStatus] = useState(null);
   const [snackbar, setSnackbar] = useState({ open: false, message: "" });
   const [frontFileName, setFrontFileName] = useState("");
   const [backFileName, setBackFileName] = useState("");
@@ -33,6 +35,7 @@ const KycForm = () => {
     const userData = JSON.parse(sessionStorage.getItem("userData"));
     if (userData && userData.user_details) {
       setUserId(userData.user_details.id);
+      setUserKycStatus(userData.user_details.kyc_status);
     }
   }, []);
 
@@ -59,7 +62,9 @@ const KycForm = () => {
       setSnackbar({ open: true, message: "Please select a valid image file." });
     }
   };
-
+  const homepage = () => {
+    Router.push("/");
+  };
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!userId) {
@@ -102,6 +107,7 @@ const KycForm = () => {
         }
       );
       setSnackbar({ open: true, message: "KYC submitted successfully!" });
+
       // Reset form and file names after successful submission
       setFormData({
         full_name: "",
@@ -111,6 +117,9 @@ const KycForm = () => {
       });
       setFrontFileName("");
       setBackFileName("");
+      setTimeout(() => {
+        homepage();
+      }, 2000);
     } catch (error) {
       console.error("Error submitting KYC:", error);
       setSnackbar({
@@ -137,84 +146,95 @@ const KycForm = () => {
           marginTop: "20px",
         }}
       >
+        {userKycStatus !== "P" && (
+          <Paper elevation={3} sx={{ p: 3, width: "80%", mx: "auto" }}>
+            <Typography variant="h5" component="h2" gutterBottom>
+              Complete KYC
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="full_name"
+                label="Full Name"
+                name="full_name"
+                placeholder="Name on document"
+                value={formData.full_name}
+                onChange={handleChange}
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="document_number"
+                label="Document Number"
+                name="document_number"
+                placeholder="ID proof number"
+                value={formData.document_number}
+                onChange={handleChange}
+              />
+              <Button
+                variant="contained"
+                component="label"
+                fullWidth
+                sx={{ mt: 2 }}
+              >
+                Document Image (Front Side)
+                <input
+                  type="file"
+                  hidden
+                  name="front_side"
+                  onChange={handleFileChange}
+                  accept="image/*"
+                />
+              </Button>
+              {frontFileName && (
+                <Typography variant="body2" sx={{ mt: 1 }}>
+                  Front side file: {frontFileName}
+                </Typography>
+              )}
+              <Button
+                variant="contained"
+                component="label"
+                fullWidth
+                sx={{ mt: 2 }}
+              >
+                Document Image (Back Side)
+                <input
+                  type="file"
+                  hidden
+                  name="back_side"
+                  onChange={handleFileChange}
+                  accept="image/*"
+                />
+              </Button>
+              {backFileName && (
+                <Typography variant="body2" sx={{ mt: 1 }}>
+                  Back side file: {backFileName}
+                </Typography>
+              )}
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                disabled={isLoading}
+              >
+                {isLoading ? <Loader size={24} /> : "Submit"}
+              </Button>
+            </Box>
+          </Paper>
+        )}{" "}
+        :
         <Paper elevation={3} sx={{ p: 3, width: "80%", mx: "auto" }}>
-          <Typography variant="h5" component="h2" gutterBottom>
-            Complete KYC
+          <Typography textAlign="center">
+            You have already submitted your KYC. We are currently awaiting admin
+            approval. Please be patient.<br></br>
+            <br></br> आपने अपना KYC पहले ही सबमिट कर दिया है। अभी प्रशासक की
+            मंजूरी का इंतजार हो रहा है। कृपया प्रतीक्षा करें।
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="full_name"
-              label="Full Name"
-              name="full_name"
-              placeholder="Name on document"
-              value={formData.full_name}
-              onChange={handleChange}
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="document_number"
-              label="Document Number"
-              name="document_number"
-              placeholder="ID proof number"
-              value={formData.document_number}
-              onChange={handleChange}
-            />
-            <Button
-              variant="contained"
-              component="label"
-              fullWidth
-              sx={{ mt: 2 }}
-            >
-              Document Image (Front Side)
-              <input
-                type="file"
-                hidden
-                name="front_side"
-                onChange={handleFileChange}
-                accept="image/*"
-              />
-            </Button>
-            {frontFileName && (
-              <Typography variant="body2" sx={{ mt: 1 }}>
-                Front side file: {frontFileName}
-              </Typography>
-            )}
-            <Button
-              variant="contained"
-              component="label"
-              fullWidth
-              sx={{ mt: 2 }}
-            >
-              Document Image (Back Side)
-              <input
-                type="file"
-                hidden
-                name="back_side"
-                onChange={handleFileChange}
-                accept="image/*"
-              />
-            </Button>
-            {backFileName && (
-              <Typography variant="body2" sx={{ mt: 1 }}>
-                Back side file: {backFileName}
-              </Typography>
-            )}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              disabled={isLoading}
-            >
-              {isLoading ? <Loader size={24} /> : "Submit"}
-            </Button>
-          </Box>
         </Paper>
       </div>
       <Snackbar
